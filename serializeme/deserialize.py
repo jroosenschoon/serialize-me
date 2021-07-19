a94c6ab43f1fbdad4e889881c03cf8469cafa85d
Jack Gisel <jackrgisel@gmail.com>
2021-07-18 17:06:43 -0700

    working prototype

--- serializeme/deserialize.py
+++ serializeme/deserialize.py
@@ -196,7 +196,7 @@ class Deserialize:
                         data.append(f)
                         index = new_index
                     all_data.append(data)
-                custom_length = str(index - old_index) + 'B'
+                custom_length = str(index - old_index) * 4
                 var_field = Field(name, custom_length, all_data)
                 self.fields.append(var_field)
             else:
@@ -219,7 +219,7 @@ class Deserialize:
                             back += 1
                         val = self.__handle_custom_formatting(
                             value_format, byte_queue)
-                        f = Field(name, str(back - index) + 'B', val)
+                        f = Field(name, (back - index) * 4, val)
                         new_index = back
                     elif format_str == PREFIX_LENGTH:
                         if value_format != None:
@@ -227,7 +227,7 @@ class Deserialize:
                                 self.packet[index:index+1], "big")
                             val = self.__handle_custom_formatting(
                                 value_format, self.packet[index:index+size+1])
-                            f = Field(name, str(size) + 'B', val)
+                            f = Field(name, (size) * 4, val)
                             new_index = index + size + 1
                         else:
                             size = int.from_bytes(
@@ -249,7 +249,7 @@ class Deserialize:
                                 c = c + 1
                             val = self.__handle_custom_formatting(
                                 value_format, byte_queue)
-                            f = Field(name, str(back - index) + 'B', val)
+                            f = Field(name, (back - index) * 4, val)
                             new_index = back
                         else:
                             (size, format) = self.__read_bit_string(format_str)
@@ -270,7 +270,7 @@ class Deserialize:
                             back += 1
                         val = self.__handle_custom_formatting(
                             value_format, byte_queue)
-                        f = Field(name, str(back - index) + 'B', val)
+                        f = Field(name, (back - index) * 4, val)
                         new_index = back
                     elif stuff == PREFIX_LENGTH:
                         size = int.from_bytes(
@@ -295,97 +295,3 @@ class Deserialize:
 
     def get_value(self, field_name):
         return self.get_field(field_name).value
-
-# TODO: Fix test cases into file lol
-
-
-# pack = Deserialize(b'\x32\xff\xff\xff\xff', {
-#     'id': '1B',
-#     "dest": ('4B', IPv4)
-# })
-# print(pack.get_field('id'))
-
-# print(pack.get_field('dest'))
-
-
-# pack = Deserialize(b'\x05\x01\x00\x03\x0ewww.google.com\x00P', {
-#     "VER": "1B",
-#     "CMD": "1B",
-#     "RSV": "1B",
-#     "ATYP": "1B",
-#     "DADDR": (PREFIX_LENGTH, HOST),
-#     "DPORT": "2B",
-# })
-
-
-# print(pack.get_field('DADDR'))
-# print(pack.get_field('DPORT'))
-# rsp = b'\x01\x06cs158b\x08Pa55word'
-
-# pck = Deserialize(rsp, {
-#     "VER": "1B",
-#     "ID": PREFIX_LENGTH,
-#     "PW": PREFIX_LENGTH
-# })
-
-# print(pck.get_field("VER").value)
-# print(pck.get_field("ID").value)
-# print(pck.get_field("PW").value)
-
-# rsp = b'\x01\x03\x00\x01\x02'
-
-# pck = Deserialize(rsp, {
-#     "VER": "1B",
-#     "NAUTHS": ('1B', "", "AUTHS"),
-#     "AUTHS": {
-#         'val': '1B'
-#     }
-# })
-
-# print(pck.get_value("VER"))
-# print(pck.get_value("NAUTHS"))
-# print(pck.get_value("AUTHS"))
-
-# pck = Deserialize(b'\x41\x33', {
-#     "!!2B": {'b0': '',
-#              'b1': '3b',
-#              'b2': '4b',
-#              'b3': '4b',
-#              'b4': '4b',
-#              }
-# })
-
-# print(pck.get_value("b0"))
-# print(pck.get_value("b1"))
-# print(pck.get_value("b2"))
-# print(pck.get_value("b3"))
-# print(pck.get_value("b4"))
-
-# # DNS example
-# dns_request = {
-#     "ID": "2B",
-#     "!!2B": {
-#         'QR': '1b',
-#         'OPCODE': '4b',
-#         'AA': '1b',
-#         'TC': '1b',
-#         'RD': '1b',
-#         'RA': '1b',
-#         "Z": "3b",
-#         "RCODE": "4b",
-#     },
-#     "QDCOUNT": ("2B", "", "QUERIES"),
-#     "ANCOUNT": "2B",
-#     "NSCOUNT": "2B",
-#     "ARCOUNT": "2B",
-#     "QUERIES": {
-#         "QNAME": PREFIX_LEN_NULL_TERM,
-#         "QTYPE": "2B",
-#         "QCLASS": "2B"
-#     }
-# }
-
-# pck = Deserialize(b'\xccD\x01 \x00\x01\x00\x00\x00\x00\x00\x01\x06google\x03com\x00\x00\x01\x00\x01\x00\x00)\x10\x00\x00\x00\x00\x00\x00\x0c\x00\n\x00\x08\xd9\xd7\xa3\xbf\xe7\xb3\xae\xb9', dns_request)
-# print(pck.get_value('QUERIES')[0][0].value)
-# pck = Deserialize(b'\x10\xf0\x01 \x00\x01\x00\x00\x00\x00\x00\x01\x011\x011\x0216\x03172\x07in-addr\x04arpa\x00\x00\x0c\x00\x01\x00\x00)\x10\x00\x00\x00\x00\x00\x00\x0c\x00\n\x00\x08\xdc\x10\xd7\xca"\xf7\xc4\xb7', dns_request)
-# print(pck.get_value('QUERIES')[0][0].value)
